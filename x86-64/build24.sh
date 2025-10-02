7d10ea76dd29b6400735d823ac8dc23c815b5157
hkl <62088749+hkelong@users.noreply.github.com>
2025-10-02 11:53:45 +0800

    Update build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -60,6 +60,8 @@ PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 PACKAGES="$PACKAGES luci-app-turboacc"
 PACKAGES="$PACKAGES luci-i18n-frpc-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-nlbwmon-zh-cn“
+PACKAGES="$PACKAGES luci-i18n-partexp-zh-cn”
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
