fb983c3e1b040bcad402f140a8693cbac50c2460
hkl <62088749+hkelong@users.noreply.github.com>
2025-10-02 11:20:38 +0800

    Update build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -61,7 +61,6 @@ PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ssr-plus-zh-cn"
 PACKAGES="$PACKAGES luci-app-turboacc"
 PACKAGES="$PACKAGES luci-i18n-frpc-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-nlbwmon-zh-cn"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
